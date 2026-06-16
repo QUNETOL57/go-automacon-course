d4bde8ae1a5055704e5b22c226f9eddb9c1738ea
QUNETOL57 <qunetol57@outlook.com>
2026-06-16 22:52:30 +0300

    lesson 2 upd

--- lesson_2/task_2_2/main.go
+++ lesson_2/task_2_2/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var first int = 16
-	var second int = 3
+	first := 16
+	second := 3
 
 	fmt.Printf("Результат: %d, остаток от деления: %d, тип результата: %T.\n", first/second, first%second, first/second)
 }
